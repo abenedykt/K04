caa81941a114424d8e3a03d0be7eec3d474c65d1
Sebastian Stasiak <s.stasiak@outlook.com>
2018-12-19 12:21:33 +0100

    refactored

--- K04/FizzBuzzTests/FizzBuzzGameTests.cs
+++ K04/FizzBuzzTests/FizzBuzzGameTests.cs
@@ -4,67 +4,18 @@ namespace FizzBuzzTests
 {
     public class FizzBuzzGameTests
     {
-        [Fact]
-        public void When_play_1_returns_1()
-        {
-            // arrange
-            var game = new FizzBuzzGame();
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(2, "2")]
+        [InlineData(3, "Fizz")]
+        [InlineData(5, "Buzz")]
+        [InlineData(15, "FizzBuzz")]
 
-            // act
-            var result = game.Play(1);
-
-            //assert
-            Assert.Equal("1", result);
-        }
-
-        [Fact]
-        public void When_play_2_returns_2()
-        {
-            // arrange
-            var game = new FizzBuzzGame();
-
-            // act
-            var result = game.Play(2);
-
-            //assert
-            Assert.Equal("2", result);
-        }
-
-        [Fact]
-        public void When_play_3_returns_Fizz()
-        {
-            // arrange
-            var game = new FizzBuzzGame();
-
-            // act
-            var result = game.Play(3);
-
-            //assert
-            Assert.Equal("Fizz", result);
-        }
-
-        [Fact]
-        public void When_play_4_returns_4()
+        public void Play_GivenInteger_ReturnsValueMatchingGameRules(int input, string expected)
         {
             var game = new FizzBuzzGame();
-            var result = game.Play(4);
-            Assert.Equal("4", result);
-        }
-
-        [Fact]
-        public void When_play_5_returns_Buzz()
-        {
-            var game = new FizzBuzzGame();
-            var result = game.Play(5);
-            Assert.Equal("Buzz", result);
-        }
-
-        [Fact]
-        public void When_play_15_returns_FizzBuzz()
-        {
-            var game = new FizzBuzzGame();
-            var result = game.Play(15);
-            Assert.Equal("FizzBuzz", result);
+            var result = game.Play(input);
+            Assert.Equal(expected, result);
         }
     }
 }
