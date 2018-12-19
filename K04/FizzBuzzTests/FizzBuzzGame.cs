40ddb9b02e6a49392875e00b684f2383e212beea
Sebastian Stasiak <s.stasiak@outlook.com>
2018-12-19 12:09:18 +0100

    Basic tests setup

---
+++ K04/FizzBuzzTests/FizzBuzzGame.cs
@@ -0,0 +1,10 @@
+﻿namespace FizzBuzzTests
+{
+    internal class FizzBuzzGame
+    {
+        internal string Play(int x)
+        {
+            return x % 3 == 0 ? x % 5 == 0 ? "FizzBuzz" : "Fizz" : x % 5 == 0 ? "Buzz" : x.ToString();
+        }
+    }
+}
---
+++ K04/FizzBuzzTests/FizzBuzzGameTests.cs
@@ -0,0 +1,70 @@
+﻿using Xunit;
+
+namespace FizzBuzzTests
+{
+    public class FizzBuzzGameTests
+    {
+        [Fact]
+        public void When_play_1_returns_1()
+        {
+            // arrange
+            var game = new FizzBuzzGame();
+
+            // act
+            var result = game.Play(1);
+
+            //assert
+            Assert.Equal("1", result);
+        }
+
+        [Fact]
+        public void When_play_2_returns_2()
+        {
+            // arrange
+            var game = new FizzBuzzGame();
+
+            // act
+            var result = game.Play(2);
+
+            //assert
+            Assert.Equal("2", result);
+        }
+
+        [Fact]
+        public void When_play_3_returns_Fizz()
+        {
+            // arrange
+            var game = new FizzBuzzGame();
+
+            // act
+            var result = game.Play(3);
+
+            //assert
+            Assert.Equal("Fizz", result);
+        }
+
+        [Fact]
+        public void When_play_4_returns_4()
+        {
+            var game = new FizzBuzzGame();
+            var result = game.Play(4);
+            Assert.Equal("4", result);
+        }
+
+        [Fact]
+        public void When_play_5_returns_Buzz()
+        {
+            var game = new FizzBuzzGame();
+            var result = game.Play(5);
+            Assert.Equal("Buzz", result);
+        }
+
+        [Fact]
+        public void When_play_15_returns_FizzBuzz()
+        {
+            var game = new FizzBuzzGame();
+            var result = game.Play(15);
+            Assert.Equal("FizzBuzz", result);
+        }
+    }
+}
