dab05d30ba89309b264e2d4b1963c0bc9986e148
Wojciech Platek (WFV) <WFV@kmd.dk>
2018-12-19 11:54:37 +0100

    Green

--- fizbaztest/Class1.cs
+++ fizbaztest/Class1.cs
@@ -10,10 +10,78 @@ namespace fizbaztest
     public class Class1
     {
         [Fact]
-        public void Dupa()  
+        public void When_1_Return_1()
         {
+            //arrange
             var game = new FizzBuzzGame();
 
+            //act
+            var result = game.Play(1);
+
+            //assert
+            Assert.Equal("1", result);
+        }
+
+        [Fact]
+        public void When_2_Return_2()
+        {
+            //arrange
+            var game = new FizzBuzzGame();
+
+            //act
+            var result = game.Play(2);
+
+            //assert
+            Assert.Equal("2", result);
+        }
+
+        [Fact]
+        public void When_3_Return_Fizz()
+        {
+            //arrange
+            var game = new FizzBuzzGame();
+
+            //act
+            var result = game.Play(3);
+
+            //assert
+            Assert.Equal("Fizz", result);
+        }
+        [Fact]
+        public void When_4_Return_4()
+        {
+            //arrange
+            var game = new FizzBuzzGame();
+
+            //act
+            var result = game.Play(4);
+
+            //assert
+            Assert.Equal("4", result);
+        }
+        [Fact]
+        public void When_5_Return_Buzz()
+        {
+            //arrange
+            var game = new FizzBuzzGame();
+
+            //act
+            var result = game.Play(5);
+
+            //assert
+            Assert.Equal("Buzz", result);
+        }
+        [Fact]
+        public void When_15_Return_FizzBuzz()
+        {
+            //arrange
+            var game = new FizzBuzzGame();
+
+            //act
+            var result = game.Play(15);
+
+            //assert
+            Assert.Equal("FizzBuzz", result);
         }
     }
 }
--- fizbaztest/FizzBuzzGame.cs
+++ fizbaztest/FizzBuzzGame.cs
@@ -1,9 +1,20 @@
-﻿namespace fizbaztest
+﻿using System;
+
+namespace fizbaztest
 {
     internal class FizzBuzzGame
     {
         public FizzBuzzGame()
         {
+        }
+
+        internal string Play(uint x)
+        {
+            if (x % 15 == 0) return "FizzBuzz";
+            if (x % 3 == 0) return "Fizz";
+            if (x % 5 == 0) return "Buzz";
+            
+            return x.ToString();
         }
     }
 }
