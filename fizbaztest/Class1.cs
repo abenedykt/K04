c7783b605d96e3a7338a676490470f5feafac8f4
Wojciech Platek (WFV) <WFV@kmd.dk>
2018-12-19 12:11:09 +0100

    Refactored.

--- fizbaztest/Class1.cs
+++ fizbaztest/Class1.cs
@@ -1,87 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace fizbaztest
 {
-    public class Class1
+    public class FizzBuzzTests
     {
-        [Fact]
-        public void When_1_Return_1()
+        private readonly FizzBuzzGame game;
+        private readonly Func<uint, string> act;
+
+        public FizzBuzzTests()
         {
-            //arrange
-            var game = new FizzBuzzGame();
-
-            //act
-            var result = game.Play(1);
-
-            //assert
-            Assert.Equal("1", result);
+            game = new FizzBuzzGame();
+            act = (i) => game.Play(i);
         }
 
-        [Fact]
-        public void When_2_Return_2()
-        {
-            //arrange
-            var game = new FizzBuzzGame();
+        [Theory, InlineData(1, "1"), InlineData(2, "2"), InlineData(3, "Fizz"), InlineData(5, "Buzz"), InlineData(15, "FizzBuzz")]
 
+        public void Returns_values_according_to_Fixx_Buzz_game_rules(uint input, string expected)
+        {
             //act
-            var result = game.Play(2);
+            var result = act(input);
 
             //assert
-            Assert.Equal("2", result);
-        }
-
-        [Fact]
-        public void When_3_Return_Fizz()
-        {
-            //arrange
-            var game = new FizzBuzzGame();
-
-            //act
-            var result = game.Play(3);
-
-            //assert
-            Assert.Equal("Fizz", result);
-        }
-        [Fact]
-        public void When_4_Return_4()
-        {
-            //arrange
-            var game = new FizzBuzzGame();
-
-            //act
-            var result = game.Play(4);
-
-            //assert
-            Assert.Equal("4", result);
-        }
-        [Fact]
-        public void When_5_Return_Buzz()
-        {
-            //arrange
-            var game = new FizzBuzzGame();
-
-            //act
-            var result = game.Play(5);
-
-            //assert
-            Assert.Equal("Buzz", result);
-        }
-        [Fact]
-        public void When_15_Return_FizzBuzz()
-        {
-            //arrange
-            var game = new FizzBuzzGame();
-
-            //act
-            var result = game.Play(15);
-
-            //assert
-            Assert.Equal("FizzBuzz", result);
+            Assert.Equal(expected, result);
         }
     }
+
 }
