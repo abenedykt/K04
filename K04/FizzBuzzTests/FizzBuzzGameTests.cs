cccde42e1d2760d4beb81c5e4ac62cca405efaf9
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2018-12-19 11:40:55 +0100

    fizz buzz game

---
+++ K04/FizzBuzzTests/FizzBuzzGameTests.cs
@@ -0,0 +1,84 @@
+﻿using Xunit;
+
+namespace FizzBuzzTests
+{
+    public class FizzBuzzGameTests
+    {
+        [Fact]
+        public void When_play_1_returns_1()
+        {
+            // arrange
+            var game = new FizzBuzzGame();
+
+            // act
+            var result = game.Play(1);
+
+            //assert
+            Assert.Equal("1", result);
+        }
+
+        [Fact]
+        public void When_play_2_returns_2()
+        {
+            // arrange
+            var game = new FizzBuzzGame();
+
+            // act
+            var result = game.Play(2);
+
+            //assert
+            Assert.Equal("2", result);
+        }
+
+        [Fact]
+        public void When_play_3_returns_Fizz()
+        {
+            // arrange
+            var game = new FizzBuzzGame();
+
+            // act
+            var result = game.Play(3);
+
+            //assert
+            Assert.Equal("Fizz", result);
+        }
+
+        [Fact]
+        public void When_play_4_returns_4()
+        {
+            var game = new FizzBuzzGame();
+            var result = game.Play(4);
+            Assert.Equal("4", result);
+        }
+
+        [Fact]
+        public void When_play_5_returns_Buzz()
+        {
+            var game = new FizzBuzzGame();
+            var result = game.Play(5);
+            Assert.Equal("Buzz", result);
+        }
+
+        [Fact]
+        public void When_play_15_returns_FizzBuzz()
+        {
+            var game = new FizzBuzzGame();
+            var result = game.Play(15);
+            Assert.Equal("FizzBuzz", result);
+        }
+
+
+    }
+
+    internal class FizzBuzzGame
+    {
+        internal string Play(int input)
+        {
+            if (input % 15 == 0) return "FizzBuzz";
+            if (input % 5 == 0) return "Buzz";
+            if (input % 3 == 0) return "Fizz";
+
+            return input.ToString();
+        }
+    }
+}
