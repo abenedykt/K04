c94f4ca585e24547019f3e38dea20aace7e582cd
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2018-12-19 17:29:02 +0100

    refactoring

--- K04/PizzaTests/OrderPriceTests.cs
+++ K04/PizzaTests/OrderPriceTests.cs
@@ -15,13 +15,13 @@ namespace Pizza
             // 4 kawałki hawajskiej (40 pln za pizze)
             // 8 kawałki 4 sery (50 za pizze)
             // 85 
-
-            var order = Substitute.For<IOrder>();
+            
             var menu = Substitute.For<IMenu>();
             menu.PizzaPrice("Pepperoni").Returns(new Price(30));
             menu.PizzaPrice("Hawajska").Returns(new Price(40));
             menu.PizzaPrice("4 Sery").Returns(new Price(50));
 
+            var order = Substitute.For<IOrder>();
             order.Positions.Returns(new[]{
                 new OrderItem("Jan", 4, "Pepperoni"),
                 new OrderItem("Mateusz", 4, "Hawajska"),
